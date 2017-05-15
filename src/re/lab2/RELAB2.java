57b1b5307e38aec68c52887d78e92fc587d72a3c
chenq <chenq@194.47.32.205>
2017-05-15 14:22:28 +0200

    2

--- src/re/lab2/RELAB2.java
+++ src/re/lab2/RELAB2.java
@@ -17,6 +17,7 @@ public class RELAB2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("1");
+        System.out.println("2");
     }
     
 }
