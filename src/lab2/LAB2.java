cdf087f15ac47d387cbdefbdf7cf073125a37865
XuantongLu <luxuantong1994@gmail.com>
2017-05-15 14:36:16 +0200

    aaa

--- src/lab2/LAB2.java
+++ src/lab2/LAB2.java
@@ -17,6 +17,7 @@ public class LAB2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("1");
+        System.out.println("aaa");
     }
     
 }
